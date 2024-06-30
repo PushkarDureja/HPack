1ebf9809b7098bbe24f0d53daa69fc9b83f85435
Pushkar Dureja <durejapushkar@gmail.com>
2024-06-30 15:09:15 +0530

    Program : Added Testcase for End to End Hpack Compression and Decompression

--- Program.cs
+++ Program.cs
@@ -4,91 +4,124 @@
     {
         static void Main(string[] args)
         {
-            HuffmanTest();
-            HpackTest1();
-            HpackTest2();
+            EndToEndHpackTest();
         }
 
-        public static void HuffmanTest()
+        public static void EndToEndHpackTest()
         {
-            //string input = "\0\u0001\u0002\u0003\u0004\u0005\u0006\u0007\u0008\u0009\n\u000B\u000C\r\u000E\u000F\u0010\u0011\u0012\u0013\u0014\u0015\u0016\u0017\u0018\u0019\u001A\u001B\u001C\u001D\u001E\u001F !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~\u007F";
-            string input = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            List<List<HeaderField>> headerList = [
+                [
+                    new(":status", "302"),
+                    new("cache-control", "private"),
+                    new("date", "Mon, 21 Oct 2013 20:13:21 GMT"),
+                    new("location", "https://www.example.com")
+                ],
+                [
+                    new(":status", "307"),
+                    new("cache-control", "private"),
+                    new("date", "Mon, 21 Oct 2013 20:13:21 GMT"),
+                    new("location", "https://www.example.com"),
+                ],
+                [
+                    new(":status", "200"),
+                    new("cache-control", "private"),
+                    new("date", "Mon, 21 Oct 2013 20:13:22 GMT"),
+                    new("location", "https://www.example.com"),
+                    new("content-encoding", "gzip"),
+                    new("set-cookie", "foo=ASDJKHQKBZXOQWEOPIUAXQWEOIU; max-age=3600; version=1"),
+                ],
+                [
+                    new(":method", "GET"),
+                    new(":scheme", "http"),
+                    new(":path", "/"),
+                    new(":authority", "www.example.com"),
+                ],
+                [
+                    new(":method", "GET"),
+                    new(":scheme", "http"),
+                    new(":path", "/"),
+                    new(":authority", "www.example.com"),
+                    new("cache-control", "no-cache"),
+                ],
+                [
+                    new(":method", "GET"),
+                    new(":scheme", "https"),
+                    new(":path", "/index.html"),
+                    new(":authority", "www.example.com"),
+                    new("custom-key", "custom-value"),
+                ]
+            ];
 
-            byte[] encodedData = Huffman.Encode(input);
-            string decodedData = Huffman.Decode(encodedData);
+            foreach (List<HeaderField> headers in headerList)
+            {
+                Console.WriteLine("### Original Headers ###");
+                PrintHeaders(headers);
 
-            Console.WriteLine(decodedData.Equals(input));
+                Hpack clientHpack = new Hpack();
+                List<byte> packedHeaders = clientHpack.Pack(headers);
+
+                Hpack serverHpack = new Hpack();
+                List<HeaderField> decodedHeaders = serverHpack.Unpack(packedHeaders);
+
+                Console.WriteLine("### Decoded Headers ###");
+                PrintHeaders(decodedHeaders);
+
+                Console.WriteLine("\r\n");
+
+                bool headersMatch = AreHeadersEqual(headers, decodedHeaders);
+                Console.WriteLine("Headers match: " + headersMatch);
+
+                bool dynamicTableMatch = AreDynamicTableEqual(clientHpack.DynamicTable, serverHpack.DynamicTable);
+                Console.WriteLine("Dynamic Table match: " + dynamicTableMatch);
+
+                Console.WriteLine("\r\n");
+            }
         }
 
-        public static void HpackTest1()
+        public static void PrintHeaders(List<HeaderField> headers)
         {
-            Hpack hpack = new Hpack();
-
-            List<HeaderField> headers = [
-                new(":method", "GET"),
-                new(":scheme", "http"),
-                new(":path", "/"),
-                new(":authority", "www.example.com"),
-            ];
-
-            byte[] packedHeaders = hpack.Pack(headers);
-            File.WriteAllBytes("encodedHeaders1", packedHeaders);
-
-            List<HeaderField> headers2 = [
-                new(":method", "GET"),
-                new(":scheme", "http"),
-                new(":path", "/"),
-                new(":authority", "www.example.com"),
-                new("cache-control", "no-cache"),
-            ];
-            byte[] packedHeaders2 = hpack.Pack(headers2);
-            File.WriteAllBytes("encodedHeaders2", packedHeaders2);
-
-            List<HeaderField> headers3 = [
-               new(":method", "GET"),
-                new(":scheme", "https"),
-                new(":path", "/index.html"),
-                new(":authority", "www.example.com"),
-                new("custom-key", "custom-value"),
-            ];
-            byte[] packedHeaders3 = hpack.Pack(headers3);
-            File.WriteAllBytes("encodedHeaders3", packedHeaders3);
+            foreach (HeaderField headerField in headers)
+            {
+                Console.WriteLine($"{headerField.Name} : {headerField.Value}");
+            }
         }
 
-        public static void HpackTest2()
+        public static bool AreHeadersEqual(List<HeaderField> originalHeaders, List<HeaderField> decodedHeaders)
         {
-            Hpack hpack = new Hpack();
-            List<HeaderField> responseHeaders = [
-                new(":status", "302"),
-                new("cache-control", "private"),
-                new("date", "Mon, 21 Oct 2013 20:13:21 GMT"),
-                new("location", "https://www.example.com"),
-            ];
+            if (originalHeaders.Count != decodedHeaders.Count)
+            {
+                return false;
+            }
 
-            byte[] packedHeaders = hpack.Pack(responseHeaders);
-            File.WriteAllBytes("encodedResponseHeaders1", packedHeaders);
+            for (int i = 0; i < originalHeaders.Count; i++)
+            {
+                if (originalHeaders[i].Name != decodedHeaders[i].Name ||
+                    originalHeaders[i].Value != decodedHeaders[i].Value)
+                {
+                    return false;
+                }
+            }
 
-            List<HeaderField> responseHeaders2 = [
-                new(":status", "307"),
-                new("cache-control", "private"),
-                new("date", "Mon, 21 Oct 2013 20:13:21 GMT"),
-                new("location", "https://www.example.com"),
-            ];
+            return true;
+        }
 
-            byte[] packedHeaders2 = hpack.Pack(responseHeaders2);
-            File.WriteAllBytes("encodedResponseHeaders2", packedHeaders2);
+        public static bool AreDynamicTableEqual(DynamicTable table1, DynamicTable table2)
+        {
+            if (table1.Count != table2.Count)
+            {
+                return false;
+            }
 
-            List<HeaderField> responseHeaders3 = [
-                new(":status", "200"),
-                new("cache-control", "private"),
-                new("date", "Mon, 21 Oct 2013 20:13:22 GMT"),
-                new("location", "https://www.example.com"),
-                new("content-encoding", "gzip"),
-                new("set-cookie", "foo=ASDJKHQKBZXOQWEOPIUAXQWEOIU; max-age=3600; version=1"),
-            ];
+            for (int i = 0; i < table1.Count; i++)
+            {
+                if (table1.GetElement(i).Name != table2.GetElement(i).Name ||
+                    table1.GetElement(i).Value != table2.GetElement(i).Value)
+                {
+                    return false;
+                }
+            }
 
-            byte[] packedHeaders3 = hpack.Pack(responseHeaders3);
-            File.WriteAllBytes("encodedResponseHeaders3", packedHeaders3);
+            return true;
         }
     }
 }
